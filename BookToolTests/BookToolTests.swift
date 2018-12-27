e4117bdfd6e5cad1eba45495a129547810dad9de
neo <wyq147258@21cn.com>
2018-12-27 10:57:47 +0800

    Initial Commit

---
+++ BookToolTests/BookToolTests.swift
@@ -0,0 +1,34 @@
+//
+//  BookToolTests.swift
+//  BookToolTests
+//
+//  Created by neo on 2018/12/27.
+//  Copyright © 2018 neo. All rights reserved.
+//
+
+import XCTest
+@testable import BookTool
+
+class BookToolTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
